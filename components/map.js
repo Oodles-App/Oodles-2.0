5cc26b5551c5bf9a27bb5c62541cabb7e6940a99
Songyee Han <hansongyee0304@gmail.com>
2022-05-12 13:42:46 -0700

    Added GeoLocation

--- components/map.js
+++ components/map.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MapContainer, TileLayer, Popup, Marker, useMap, useLeaflet} from 'react-leaflet';
-import { useState } from 'react';
+import { useState, useRef} from 'react';
+// import LocateControl from 'react-leaflet-locate-control'
+
 
 
 export default function Map(props) {
   const [restaurants, setRestaurants] = useState(props.restaurants)
-  // console.log("props here", restaurants)
-
+  
   return (
+    <div>
+
+
     <div id="map" >
       <MapContainer center={[40.735360, -73.989970]} zoom={11} scrollWheelZoom={false} >
       <TileLayer
@@ -15,10 +19,11 @@ export default function Map(props) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           accessToken='pk.eyJ1IjoiYnVzdWh0ODMiLCJhIjoiY2wzMXE5NmdvMjIzMjNsbXV2bmdocXduMiJ9.xgMMsvbz_5VqqpadcYpmzg'
       />
-      <Markers data={restaurants}/>
+      {/* <Markers data={restaurants}/> */}
+      <LocationMarker/>
       </MapContainer>
     </div>
-    
+    </div>
   )
 }
 
@@ -56,5 +61,36 @@ function Markers( {data} ) {
         </Marker>
       );
     })
+  );
+}
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const [bbox, setBbox] = useState([]);
+
+  const map = useMap();
+
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+      const radius = e.accuracy;
+      const circle = L.circle(e.latlng, radius);
+      circle.addTo(map);
+      setBbox(e.bounds.toBBoxString().split(","));
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>
+        You are here. <br />
+        Map bbox: <br />
+        <b>Southwest lng</b>: {bbox[0]} <br />
+        <b>Southwest lat</b>: {bbox[1]} <br />
+        <b>Northeast lng</b>: {bbox[2]} <br />
+        <b>Northeast lat</b>: {bbox[3]}
+      </Popup>
+    </Marker>
   );
 }
