50153df8e71df6c43a7a2cdcfce503ccff28c736
Josie Icaza <jocelyn.icaza@gmail.com>
2022-05-15 12:10:44 -0700

    buggy navbar

--- components/Layout.js
+++ components/Layout.js
@@ -10,34 +10,30 @@ export default function Layout({children}) {
 
   return (
     <>
-      <header className={styles.header}>
+        <header className={styles.header}>
           <nav className={styles.navbar}>
               <Link href='/home'>
                 <a className={styles.navlogo}>Oodles</a>
               </Link>
-            {
-              isOpen && (
-                <ul className={styles.navmenu}>
-                  <li className={styles.navitem}>
-                      <Link href='/home'>
-                        <a onClick={() => setOpen(false)} className={styles.navlink}>Home</a>
-                      </Link>
-                  </li>
-                  <li className={styles.navitem}>
-                      <Link href='/browse'>
-                        <a onClick={() => setOpen(false)} className={styles.navlink}>Browse</a>
-                      </Link>
+            <ul className={isOpen === false ?
+                        styles.navmenu : styles.navmenu +' '+ styles.active}>
+              <li className={styles.navitem}>
+                  <Link href='/home'>
+                    <a onClick={() => setOpen(false)} className={styles.navlink}>Home</a>
+                  </Link>
+              </li>
+              <li className={styles.navitem}>
+                  <Link href='/browse'>
+                    <a onClick={() => setOpen(false)} className={styles.navlink}>Browse</a>
+                  </Link>
 
-                  </li>
-                  <li className={styles.navitem}>
-                      <Link href='/articles'>
-                        <a onClick={() => setOpen(false)} className={styles.navlink}>Articles</a>
-                      </Link>
-                  </li>
-                </ul>
-              )
-            }
-
+              </li>
+              <li className={styles.navitem}>
+                  <Link href='/articles'>
+                    <a onClick={() => setOpen(false)} className={styles.navlink}>Articles</a>
+                  </Link>
+              </li>
+            </ul>
             <button className={isOpen === false ?
                                     styles.hamburger : styles.hamburger +' '+ styles.active}
                                     onClick={clickMenu}
