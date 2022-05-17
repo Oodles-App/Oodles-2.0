43344b1405f6afa2b6c07f052e23644adfdf62fe
Stephanie Powell <spowell519@gmail.com>
2022-05-17 13:48:29 -0400

    Get tags in nonProfitOrg pg + moved livechat to nonProfitOrg pg

--- pages/browse/nonProfitOrg/[id].js
+++ pages/browse/nonProfitOrg/[id].js
@@ -4,6 +4,11 @@ import Link from 'next/link';
 const prisma = new PrismaClient();
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import dynamic from "next/dynamic";
+
+const LiveChat = dynamic(() => import("../../../components/liveChat/LiveChat"), {
+  ssr: false,
+});
 
 export const getServerSideProps = async({params}) => {
     const organization = await prisma.user.findUnique({
@@ -11,19 +16,23 @@ export const getServerSideProps = async({params}) => {
             id: parseInt(params.id)
         }
     })
-    console.log(organization)
+    const tags = await prisma.tag.findMany({
+        include: {
+            users: true
+        }
+    })
     return {
         props: {
-           organizationInfo: JSON.parse(JSON.stringify(organization))
+           organizationInfo: JSON.parse(JSON.stringify(organization)),
+           tags: JSON.parse(JSON.stringify(tags)).filter((tag) => tag.users.some((user) => user.id === +params.id))
         },
     }
 
 }
 
-const Organization = ({organizationInfo}) => {
+const Organization = ({organizationInfo, tags}) => {
     const [organization, setOrganizations] = useState(organizationInfo)
     const router = useRouter();
-
     return (
         <div>
             <div>
@@ -34,9 +43,10 @@ const Organization = ({organizationInfo}) => {
                 <div>
                     <p>Bio:</p>
                 </div>
+                {tags.map((tag) => <p key={tag.id}>{tag.label}</p>)}
             </div>
             <button type="button" style={{border:"1px solid black"}} onClick={() => {router.push("/browse")}}>Back</button>
-
+            <LiveChat />
         </div>
         
     )
