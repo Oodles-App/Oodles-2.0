ce98cfbb688b071bc23f123f8c671205dbf19176
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-05-17 12:29:41 -0400

    remove old files related to old dummy db

--- pages/api/index.js
+++
@@ -1,9 +0,0 @@
-export default apiHandler({
-  get: getUsers,
-});
-
-function getUsers(req, res) {
-  // return users without hashed passwords in the response
-  const response = usersRepo.getAll().map((x) => omit(x, "hash"));
-  return res.status(200).json(cars);
-}
