00ee6ce788ce21c9bcf1b9dfb2dfe0f0f72abf49
Stephanie Powell <spowell519@gmail.com>
2022-05-17 12:25:32 -0400

    Fixed conflict issues with _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,10 @@ import { Alert } from "../components/Alert";
 import { wrapper, store, Persistor } from "../redux/store";
 import { Provider } from "react-redux";
 import "../styles/globals.css";
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 import Head from "next/head";
-
-<<<<<<< HEAD
 import Script from "next/script";
 
-
-
-import Layout from "../components/Layout";
-
-=======
->>>>>>> main
 import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }) {
@@ -27,19 +19,18 @@ function MyApp({ Component, pageProps }) {
               name="description"
               content="Donate Food to Nonprofit Organizations"
             />
-            <meta name="google-signin-client_id" content="304868782951-o0gqd2osf1fcjbhv9ibf63r6lru98p36.apps.googleusercontent.com"/>
+            <meta
+              name="google-signin-client_id"
+              content="304868782951-o0gqd2osf1fcjbhv9ibf63r6lru98p36.apps.googleusercontent.com"
+            />
             <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/utensilHeart.png" />
           </Head>
-<<<<<<< HEAD
-          <Alert />
-          <Script src="https://apis.google.com/js/platform.js"/>
-          <Component {...pageProps} />
-=======
           <Layout>
             <Alert />
             <Component {...pageProps} />
           </Layout>
->>>>>>> main
+          <Script src="https://apis.google.com/js/platform.js"/>
         </PersistGate>
       </Provider>
     </>
