e43326a6ff36570cc5a90c3007c0a9dd70f34b35
SongyeeHan000 <93545868+SongyeeHan000@users.noreply.github.com>
2022-05-18 12:53:40 -0700

    Revert "Reservation"

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -13,30 +13,17 @@ export async function getStaticProps() {
       },
     },
   });
-  const organizations = await prisma.user.findMany({
-    where:{ 
-      businessType: {
-        equals: 'organization'
-      }
-    }
-  });
   return {
     props: {
       initialRestaurants: JSON.parse(JSON.stringify(restaurants)),
-      initialOrganizations: JSON.parse(JSON.stringify(organizations))
-    }
-  }
+    },
+  };
 }
 
-function Browse({initialRestaurants, initialOrganizations}) {
-  const [restaurants, setRestaurants] = useState(initialRestaurants)
-  const [organizations, setOrganizations] = useState(initialOrganizations)
-  const [toggleMap, setToggleMap] = useState(true)
-  const [filteredResults, setFilteredResults] = useState(initialRestaurants)
-  const [display, setDisplay] = useState("Restaurants")
-  const user = useSelector((state) => state.user); 
-  console.log(organizations)
-
+function Browse({ initialRestaurants }) {
+  const [restaurants, setRestaurants] = useState(initialRestaurants);
+  const [toggleMap, setToggleMap] = useState(true);
+  const [filteredResults, setFilteredResults] = useState(initialRestaurants);
 
   function searchRestaurant(value) {
     if (value !== "") {
@@ -51,82 +38,62 @@ function Browse({initialRestaurants, initialOrganizations}) {
       setFilteredResults(restaurants);
     }
   }
-  function handleChange(e) {
-    setDisplay(e.target.value)
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-  
   return (
     <div>
-      <div>
       <br></br>
       <div className="Browse">
-        <h1 style={{"textAlign":"center"}}>Browse</h1>
+        <h1 style={{ textAlign: "center" }}>Browse</h1>
         <br></br>
         <p>Location: New York, NY</p>
         <br></br>
       </div>
       <div className="browseButtons">
-        <button id="mapId" type="button" name="Map" onClick={()=> {setToggleMap(true)}}>Map</button>
-        <button  id="list" type="button" name="List" onClick={() => {setToggleMap(false)}}>List</button>
+        <button
+          id="mapId"
+          type="button"
+          name="Map"
+          onClick={() => {
+            setToggleMap(true);
+          }}
+        >
+          Map
+        </button>
+        <button
+          id="list"
+          type="button"
+          name="List"
+          onClick={() => {
+            setToggleMap(false);
+          }}
+        >
+          List
+        </button>
       </div>
       <br></br>
-          <form action="#" style={{textAlign:"center"}} onSubmit={handleSubmit}>
-                  <label htmlFor='uses'>Select:</label>
-                  <select value={display} onChange={handleChange}>
-                    <option value="Restaurants">Restaurants</option>
-                    <option value="Organizations">Organizations</option>
-                  </select>
-          <input type="submit"/>
-          </form>
-      <br></br>
-      {display === "Restaurants" ? (
-        //restaurants 
+      <div>
+        {toggleMap ? (
           <div>
-          {toggleMap ? <div><Map restaurants={restaurants}></Map> </div>
-          : <div>
-            <input id="search" type="text" placeholder="Search by Restaurant" name="search" onChange={(e)=>searchRestaurant(e.target.value)} />
+            <Map restaurants={restaurants}></Map>{" "}
+          </div>
+        ) : (
+          <div>
+            <input
+              id="search"
+              type="text"
+              placeholder="Search by Restaurant"
+              name="search"
+              onChange={(e) => searchRestaurant(e.target.value)}
+            />
             <div>
               <br></br>
               {filterRestaurants(filteredResults)}
             </div>
-            </div>
-            }
-        </div>
-      ) : (
-        <p>Sorry, no organizations to show yet</p>      
-        //organizations
-        
-        // <div>
-        //   {toggleMap ? <div><Map organizations={organizations}></Map> </div>
-        //   : <div>
-        //     <input id="search" type="text" placeholder="Search by Organization" name="search" />
-        //     <div>
-        //       {/* search bar for organization doesn't work yet */}
-        //       <br></br>
-        //       {/* organizations not showing up */}
-        //       <div>
-        //         {organizations.map((organization) => {
-        //             <Link href="/browse/nonProfitOrg/[id]" as={`/browse/nonProfitOrg/${organization.id}`} key={organization.id} organization={organization.id}>
-        //             <p key={organization.id}>{organization.businessName}</p>
-        //             </Link> 
-        //         })
-        //         }
-        //       </div>
-        //     </div>
-        //     </div>
-        //     }
-        // </div>
-    
-      )}
-      
+          </div>
+        )}
       </div>
     </div>
-    
-  )
+  );
 }
 
 function filterRestaurants(filteredResults) {
