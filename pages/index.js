d37094f3c95b7a8885c99bb0bcb73b44a94c1e62
Stephanie Powell <spowell519@gmail.com>
2022-05-19 17:40:58 -0400

    Landing page updated

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import { logout } from "../redux/user";
-import { useDispatch } from "react-redux";
 import Home from './home';
 
 export default function Landing() {
-  const dispatch = useDispatch();
   return (
     <Home />
   );
