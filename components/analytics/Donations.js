9370121b7c528b298d6b2bca6598da07e4234e9f
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-05-22 15:01:10 -0400

    break donations chart

--- components/analytics/Donations.js
+++ components/analytics/Donations.js
@@ -7,12 +7,41 @@ import styles from "../../styles/Analytics.module.css";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
+// const colorGenMonths = (donations) => {
+//   switch (donations) {
+//     case donations < 10:
+//       return "low";
+//     case donations < 20:
+//       return "med";
+//     case donations < 30:
+//       return "high";
+//     case donations >= 30:
+//       return "highest";
+//     default:
+//       return "none";
+//   }
+// };
+
 // PAST YEAR  or n months: aggregate quantity of donations by month
+// const dummyDataMonths = [
+//   { month: "December", donations: 14, value: colorGenMonths(this.donations) },
+//   {
+//     month: "January",
+//     donations: 10,
+//     value: colorGenMonths(this.donations),
+//   },
+//   { month: "February", donations: 23, value: colorGenMonths(this.donations) },
+//   { month: "March", donations: 12, value: colorGenMonths(this.donations) },
+//   { month: "April", donations: 32, value: colorGenMonths(this.donations) },
+//   { month: "May", donations: 45, value: colorGenMonths(this.donations) },
+// ];
+
 const dummyDataMonths = [
   { month: "December", donations: 14 },
   {
     month: "January",
     donations: 10,
+    value: colorGenMonths(this.donations),
   },
   { month: "February", donations: 23 },
   { month: "March", donations: 12 },
@@ -20,48 +49,66 @@ const dummyDataMonths = [
   { month: "May", donations: 45 },
 ];
 
+console.log(dummyDataMonths);
+
 // PAST WEEK aggregate quantity by day (etc.)
-const dummyDataWeek = [
-  {
-    day: "Tuesday",
-    donations: 5,
-  },
-  {
-    day: "Wednesday",
-    donations: 5,
-  },
-  {
-    day: "Thursday",
-    donations: 10,
-  },
-  {
-    day: "Friday",
-    donations: 7,
-  },
-  {
-    day: "Saturday",
-    donations: 0,
-  },
-  {
-    day: "Sunday",
-    donations: 0,
-  },
-  {
-    day: "Monday",
-    donations: 5,
-  },
-];
+// const dummyDataWeek = [
+//   {
+//     day: "Tuesday",
+//     donations: 5,
+//     value: colorGenMonths(this.donations),
+//   },
+//   {
+//     day: "Wednesday",
+//     donations: 5,
+//     value: colorGenMonths(this.donations),
+//   },
+//   {
+//     day: "Thursday",
+//     donations: 10,
+//     value: colorGenMonths(this.donations),
+//   },
+//   {
+//     day: "Friday",
+//     donations: 7,
+//     value: colorGenMonths(this.donations),
+//   },
+//   {
+//     day: "Saturday",
+//     donations: 0,
+//     value: colorGenMonths(this.donations),
+//   },
+//   {
+//     day: "Sunday",
+//     donations: 0,
+//     value: colorGenMonths(this.donations),
+//   },
+//   {
+//     day: "Monday",
+//     donations: 5,
+//     value: colorGenMonths(this.donations),
+//   },
+// ];
 
 const config = {
   guide: {
     x: { label: `Time (Mo.)` },
     y: { label: "Donations" },
     padding: { b: 40, l: 40, t: 10, r: 10 },
+    // color: {
+    //   brewer: {
+    //     low: "rgba(228, 33, 85, 0.41)",
+    //     med: "rgba(228, 33, 85, 0.6)",
+    //     high: "rgba(228, 33, 85, 0.8)",
+    //     highest: "#e42155",
+    //   },
+    // },
   },
-  data: dummyDataWeek,
+  data: dummyDataMonths,
   type: "bar",
-  x: "day",
+  x: "month",
   y: "donations",
+  // color: "value",
   settings: {
     fitModel: "normal",
     animationSpeed: 1000,
@@ -73,21 +120,32 @@ const chart = new Taucharts.Chart(config);
 
 const Donations = () => {
   const [loading, setLoading] = useState(true);
-  const [time, setTime] = useState("day");
+  // const [time, setTime] = useState("day");
   const domRef = useRef();
 
   useEffect(() => {
-    const data = time === "day" ? dummyDataWeek : dummyDataMonths;
+    console.log("inside use effect");
+    const data = dummyDataMonths;
+    // const data = time === "day" ? dummyDataWeek : dummyDataMonths;
     chart.updateConfig({
       guide: {
-        x: { label: `Time (Mo.)` },
+        x: { label: `Time` },
         y: { label: "Donations" },
         padding: { b: 40, l: 40, t: 10, r: 10 },
+        color: {
+          brewer: {
+            low: "rgba(228, 33, 85, 0.41)",
+            med: "rgba(228, 33, 85, 0.6)",
+            high: "rgba(228, 33, 85, 0.8)",
+            highest: "#e42155",
+          },
+        },
       },
-      data: data,
+      data: dummyDataMonths,
       type: "bar",
-      x: time,
+      x: "month",
       y: "donations",
+      color: "value",
       settings: {
         fitModel: "normal",
         animationSpeed: 1000,
@@ -97,6 +155,7 @@ const Donations = () => {
   }, [time]);
 
   useEffect(() => {
+    console.log("inside first useEffect");
     if (typeof window !== "undefined") {
       setLoading(false);
     }
