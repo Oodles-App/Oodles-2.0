96646af4d5d1567a826837e2720b42b85e25a33b
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-05-22 22:34:04 -0400

    style edit profile page

--- pages/account/edit-profile.jsx
+++ pages/account/edit-profile.jsx
@@ -5,7 +5,7 @@ import Select from "react-select";
 import { Layout } from "../../components/account";
 import { createAlert } from "../../redux/alerts";
 import { fetchEditProfile } from "../../redux/profile";
-import { TextField, TextareaAutosize } from "@mui/material";
+import { TextField, TextareaAutosize, Button } from "@mui/material";
 import ChartLoading from "../../components/analytics/ChartLoading";
 
 // import Image from "next/image"; // => TODO: figure out optimization for using Next Image
@@ -16,6 +16,9 @@ import { fetchTags } from "../../redux/tags";
 //TODO: validation and error handling => don't toggle submit unless update was successful
 //TODO: address auto complete?
 //TODO (stretch): allow users to upload image instead of using url?
+
+import { editProfileTheme } from "../../styles/MuiThemes";
+import { ThemeProvider } from "@mui/material";
 
 const EditProfile = () => {
   const dispatch = useDispatch();
@@ -126,112 +129,138 @@ const EditProfile = () => {
   }
 
   return (
-    <Layout>
-      <div className={styles.imageContainer}>
-        <img
-          src={profile.imageUrl || placeholderSrc}
-          className={styles.image}
-        />
-        {/* Next.js image element doesn't allow external URls except those that are denoted in next.config
+    <ThemeProvider theme={editProfileTheme}>
+      <div className={`pt-5 max-w-2xl my-0 mx-auto ${styles.editWrapper}`}>
+        <Layout>
+          <div className={styles.imageContainer}>
+            <img
+              src={profile.imageUrl || placeholderSrc}
+              className={styles.image}
+            />
+            {/* Next.js image element doesn't allow external URls except those that are denoted in next.config
         <Image
           src={profile.imageUrl || placeholderSrc}
           alt="User profile image"
           layout="fill"
           objectFit="cover"
         /> */}
-      </div>
-      <form onSubmit={handleFormSubmit} className={styles.form}>
-        <div>
-          <h3 className={styles.title}>Edit Profile</h3>
-          <hr />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="businessName" className={styles.label}>
-            Business Name:
-          </label>
-          <TextField
-            name="businessName"
-            type="text"
-            value={formContent.businessName}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="email" className={styles.label}>
-            Email:
-          </label>
-          <TextField
-            name="email"
-            type="email"
-            value={formContent.email}
-            autoComplete="new-password"
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="contactNum" className={styles.label}>
-            Phone Number:
-          </label>
-          <TextField
-            name="contactNum"
-            type="text"
-            value={formContent.contactNum}
-            required
-            maxLength={10}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="address" className={styles.label}>
-            Address:
-          </label>
-          <TextField
-            name="address"
-            type="text"
-            value={formContent.address}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="biography" className={styles.label}>
-            Biography:
-          </label>
-          <TextareaAutosize
-            name="biography"
-            minRows={3}
-            value={formContent.biography || ""}
-            onChange={handleFormChange}
-            className={styles.textarea}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="imageUrl" className={styles.label}>
-            Profile Image (Link)
-          </label>
-          <TextField
-            name="imageUrl"
-            value={formContent.imageUrl || ""}
-            onChange={handleFormChange}
-          />
-        </div>
-        {(user.businessType === "ORGANIZATION" ||
-          user.businessType === "organization") && (
-          <div className={styles.formGroup}>
-            <label htmlFor="tags">What are your most-needed items? (6)</label>
-            <Select
-              isMulti
-              options={allTags}
-              name="tags"
-              value={orgTags}
-              onChange={(tags) => setOrgTags(tags)}
-            />
           </div>
-        )}
-        <button type="submit" disabled={!newChanges}>
-          {newChanges ? "Save Changes" : "Changes Saved"}
-        </button>
-      </form>
-    </Layout>
+          <form onSubmit={handleFormSubmit} className={styles.form}>
+            <div>
+              <h3 className={`${styles.title} text-2xl font-bold`}>
+                Edit Profile
+              </h3>
+              <hr className="border-1 border-black" />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="businessName" className={styles.label}>
+                Business Name:
+              </label>
+              <TextField
+                name="businessName"
+                type="text"
+                className="bg-slate-50"
+                sx={{ borderRadius: 1 }}
+                value={formContent.businessName}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="email" className={styles.label}>
+                Email:
+              </label>
+              <TextField
+                name="email"
+                type="email"
+                className="bg-slate-50"
+                sx={{ borderRadius: 1 }}
+                value={formContent.email}
+                autoComplete="new-password"
+                onChange={handleFormChange}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="contactNum" className={styles.label}>
+                Phone Number:
+              </label>
+              <TextField
+                name="contactNum"
+                type="text"
+                className="bg-slate-50"
+                sx={{ borderRadius: 1 }}
+                value={formContent.contactNum}
+                required
+                maxLength={10}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="address" className={styles.label}>
+                Address:
+              </label>
+              <TextField
+                name="address"
+                type="text"
+                className="bg-slate-50"
+                sx={{ borderRadius: 1 }}
+                value={formContent.address}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="biography" className={styles.label}>
+                Biography:
+              </label>
+              <TextareaAutosize
+                name="biography"
+                minRows={3}
+                sx={{ borderRadius: 2 }}
+                value={formContent.biography || ""}
+                onChange={handleFormChange}
+                className={`${styles.textarea} bg-slate-50`}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="imageUrl" className={styles.label}>
+                Profile Image (Link)
+              </label>
+              <TextField
+                name="imageUrl"
+                className="bg-slate-50"
+                sx={{ borderRadius: 1 }}
+                value={formContent.imageUrl || ""}
+                onChange={handleFormChange}
+              />
+            </div>
+            {(user.businessType === "ORGANIZATION" ||
+              user.businessType === "organization") && (
+              <div className={styles.formGroup}>
+                <label htmlFor="tags">
+                  What are your most-needed items? (6)
+                </label>
+                <Select
+                  color="primary"
+                  isMulti
+                  options={allTags}
+                  name="tags"
+                  className={`bg-slate-50 ${styles.select}`}
+                  value={orgTags}
+                  sx={{ borderRadius: 1 }}
+                  onChange={(tags) => setOrgTags(tags)}
+                />
+              </div>
+            )}
+            <div className="my-0 mx-auto w-auto">
+              <Button type="submit" variant="contained" disabled={!newChanges}>
+                <span className="text-white">
+                  {newChanges ? "Save Changes" : "Changes Saved"}
+                </span>
+              </Button>
+            </div>
+          </form>
+        </Layout>
+      </div>
+    </ThemeProvider>
   );
 };
 
---
+++ styles/MuiThemes.js
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material";
+
+export const chartsTheme = createTheme({
+  palette: {
+    primary: {
+      light: "#0fffff",
+      main: "#E42256",
+      dark: "#00bdbd",
+    },
+    secondary: {
+      light: "#E42256",
+      main: "#E42256",
+      dark: "#E42256",
+    },
+  },
+});
+
+export const editProfileTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#00B1B0",
+      dark: "#00B1B0",
+    },
+    secondary: {
+      main: "#E42256",
+      dark: "#E42256",
+    },
+  },
+  select: {
+    "&:before": {
+      borderColor: "#00B1B0",
+    },
+    "&:after": {
+      borderColor: "#00B1B0",
+    },
+  },
+});
