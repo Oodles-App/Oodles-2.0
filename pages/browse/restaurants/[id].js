71a564232cc17df3a1e306da2b58b0c795e8c768
Josie Icaza <jocelyn.icaza@gmail.com>
2022-05-24 07:39:10 -0700

    env missing

--- pages/browse/restaurants/[id].js
+++ pages/browse/restaurants/[id].js
@@ -43,8 +43,12 @@ const Restaurant = ({ restaurantInfo, initialProducts }) => {
         <p>{restaurant.address}</p>
         <p>{restaurant.contactNum}</p>
         <div>
-          <p>Bio:</p>
-          <p>Products: </p>
+          <p>{restaurant.biography}</p>
+          <br></br>
+          {products ==! null ? (
+            <p>Products: </p>
+          ): "Sorry, no products available at the moment"
+          }
           <ul>
               { products ? products.map((product) => (
                   <li key={product.id}>{product.name}</li>
@@ -53,6 +57,7 @@ const Restaurant = ({ restaurantInfo, initialProducts }) => {
           </ul>
         </div>
       </div>
+      <br></br>
       <button
         type="button"
         style={{ border: "1px solid black" }}
@@ -63,7 +68,13 @@ const Restaurant = ({ restaurantInfo, initialProducts }) => {
         Back
       </button>
       <div></div>
-      <Link href={{pathname: `/reservation/${restaurant.id}`}} style={{border:"1px solid black"}}>Reserve</Link>
+
+      <Link href={"/reservation/[id]"} as={`/reservation/${restaurant.id}`}>
+          <button type="button" style={{border:"1px solid black"}}>
+            Reserve
+          </button>
+      </Link>
+
       <button
         type="button"
         style={{ border: "1px solid black" }}
