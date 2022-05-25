46d4b3643532d4dc719f09f31b181d08db34bddd
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-05-25 00:49:22 -0400

    fix typo bug

--- prisma/data/reservations.js
+++ prisma/data/reservations.js
@@ -3,7 +3,7 @@ const randomRestaurantId = () => {
 };
 
 function randomDate() {
-  const start = new Date(20100, 0, 1);
+  const start = new Date(2021, 0, 1);
   const end = new Date();
   return new Date(
     start.getTime() + Math.random() * (end.getTime() - start.getTime())
